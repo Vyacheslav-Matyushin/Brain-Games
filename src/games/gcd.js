105d2d01b1f810737c882e2ac98dc5d2cfbc2f9c
Vyacheslav-Matyushin <105803009+Vyacheslav-Matyushin@users.noreply.github.com>
2023-12-10 17:34:16 +0700

    add bin/brain-gcd.js, src/games/gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,30 @@
+import runEngine from '../index.js';
+import generateRandomNumber from '../helper.js';
+
+const getGcd = (a, b) => {
+  let num1 = a;
+  let num2 = b;
+  while (num2 !== 0) {
+    const temp = num2;
+    num2 = num1 % num2;
+    num1 = temp;
+  }
+  return num1;
+};
+
+const generateRound = () => {
+  const number1 = generateRandomNumber(0, 100);
+  const number2 = generateRandomNumber(0, 100);
+  const example = `${number1} ${number2}`;
+
+  const answer = String(getGcd(number1, number2));
+  return [example, answer];
+};
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const runGcd = () => {
+  runEngine(description, generateRound);
+};
+
+export default runGcd;
